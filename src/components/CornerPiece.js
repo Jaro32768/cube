417e41b9e0a45d8477cc44944a91eb672bd648b3
Jaro <jarko04@hotmail.sk>
2022-08-05 22:50:07 +0200

    implement corner pieces

--- src/components/CornerPiece.js
+++ src/components/CornerPiece.js
@@ -3,6 +3,10 @@ import PieceSide from './PieceSide';
 
 export default function CornerPiece(props) {
     return (
-        <PieceSide color={props.color} position={[0, 0, 0]} />
+        <>
+            <PieceSide color={props.color} position={props.position1} rotation={[Math.PI / 2, 0, 0]} />
+            <PieceSide color={props.color} position={props.position2} rotation={[0, Math.PI / 2, 0]} />
+            <PieceSide color={props.color} position={props.position3} rotation={[0, 0, Math.PI / 2]} />
+        </>
     )
 }
--- src/components/Cube3x3x3.js
+++ src/components/Cube3x3x3.js
@@ -4,34 +4,39 @@ import EdgePiece from './EdgePiece';
 import CornerPiece from './CornerPiece';
 
 export default function Cube3x3x3() {
-    return (<>
-        <mesh rotation={[0, 0, 0]} position={[0, 0, 0]}>
-            <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
-            <meshLambertMaterial attach='material' color={'white'} />
-        </mesh>
-        {/*<CenterPiece position={[0, 0, 1.5]} rotation={[0, Math.PI / 2, 0]} />
-        <CenterPiece position={[0, 0, -1.5]} rotation={[0, Math.PI / 2, 0]} />
-        <CenterPiece position={[0, 1.5, 0]} rotation={[0, 0, Math.PI / 2]} />
-        <CenterPiece position={[0, -1.5, 0]} rotation={[0, 0, Math.PI / 2]} />
-        <CenterPiece position={[1.5, 0, 0]} rotation={[0, 0, 0]} />
-        <CenterPiece position={[-1.5, 0, 0]} rotation={[0, 0, 0]} />
+    return (
+        <>
+            <CenterPiece position={[0, 0, 1.5]} rotation={[0, Math.PI / 2, 0]} />
+            <CenterPiece position={[0, 0, -1.5]} rotation={[0, Math.PI / 2, 0]} />
+            <CenterPiece position={[0, 1.5, 0]} rotation={[0, 0, Math.PI / 2]} />
+            <CenterPiece position={[0, -1.5, 0]} rotation={[0, 0, Math.PI / 2]} />
+            <CenterPiece position={[1.5, 0, 0]} rotation={[0, 0, 0]} />
+            <CenterPiece position={[-1.5, 0, 0]} rotation={[0, 0, 0]} />
 
-        <EdgePiece position1={[1.5, 1, 0]} position2={[1, 1.5, 0]} rotation1={[0, 0, 0]} rotation2={[0, 0, Math.PI / 2]} />
-        <EdgePiece position1={[-1.5, 1, 0]} position2={[-1, 1.5, 0]} rotation1={[0, 0, 0]} rotation2={[0, 0, Math.PI / 2]} />
-        <EdgePiece position1={[1.5, -1, 0]} position2={[1, -1.5, 0]} rotation1={[0, 0, 0]} rotation2={[0, 0, Math.PI / 2]} />
-        <EdgePiece position1={[-1.5, -1, 0]} position2={[-1, -1.5, 0]} rotation1={[0, 0, 0]} rotation2={[0, 0, Math.PI / 2]} />
+            <EdgePiece position1={[1.5, 1, 0]} position2={[1, 1.5, 0]} rotation1={[0, 0, 0]} rotation2={[0, 0, Math.PI / 2]} />
+            <EdgePiece position1={[-1.5, 1, 0]} position2={[-1, 1.5, 0]} rotation1={[0, 0, 0]} rotation2={[0, 0, Math.PI / 2]} />
+            <EdgePiece position1={[1.5, -1, 0]} position2={[1, -1.5, 0]} rotation1={[0, 0, 0]} rotation2={[0, 0, Math.PI / 2]} />
+            <EdgePiece position1={[-1.5, -1, 0]} position2={[-1, -1.5, 0]} rotation1={[0, 0, 0]} rotation2={[0, 0, Math.PI / 2]} />
 
-        <EdgePiece position1={[1.5, 0, 1]} position2={[1, 0, 1.5]} rotation1={[0, 0, 0]} rotation2={[0, Math.PI / 2, 0]} />
-        <EdgePiece position1={[-1.5, 0, 1]} position2={[-1, 0, 1.5]} rotation1={[0, 0, 0]} rotation2={[0, Math.PI / 2, 0]} />
-        <EdgePiece position1={[1.5, 0, -1]} position2={[1, 0, -1.5]} rotation1={[0, 0, 0]} rotation2={[0, Math.PI / 2, 0]} />
-        <EdgePiece position1={[-1.5, 0, -1]} position2={[-1, 0, -1.5]} rotation1={[0, 0, 0]} rotation2={[0, Math.PI / 2, 0]} />
+            <EdgePiece position1={[1.5, 0, 1]} position2={[1, 0, 1.5]} rotation1={[0, 0, 0]} rotation2={[0, Math.PI / 2, 0]} />
+            <EdgePiece position1={[-1.5, 0, 1]} position2={[-1, 0, 1.5]} rotation1={[0, 0, 0]} rotation2={[0, Math.PI / 2, 0]} />
+            <EdgePiece position1={[1.5, 0, -1]} position2={[1, 0, -1.5]} rotation1={[0, 0, 0]} rotation2={[0, Math.PI / 2, 0]} />
+            <EdgePiece position1={[-1.5, 0, -1]} position2={[-1, 0, -1.5]} rotation1={[0, 0, 0]} rotation2={[0, Math.PI / 2, 0]} />
 
-        <EdgePiece position1={[0, 1, 1.5]} position2={[0, 1.5, 1]} rotation1={[0, Math.PI / 2, 0]} rotation2={[0, 0, Math.PI / 2]} />
-        <EdgePiece position1={[0, 1, -1.5]} position2={[0, 1.5, -1]} rotation1={[0, Math.PI / 2, 0]} rotation2={[0, 0, Math.PI / 2]} />
-        <EdgePiece position1={[0, -1, 1.5]} position2={[0, -1.5, 1]} rotation1={[0, Math.PI / 2, 0]} rotation2={[0, 0, Math.PI / 2]} />
-        <EdgePiece position1={[0, -1, -1.5]} position2={[0, -1.5, -1]} rotation1={[0, Math.PI / 2, 0]} rotation2={[0, 0, Math.PI / 2]} />*/}
+            <EdgePiece position1={[0, 1, 1.5]} position2={[0, 1.5, 1]} rotation1={[0, Math.PI / 2, 0]} rotation2={[0, 0, Math.PI / 2]} />
+            <EdgePiece position1={[0, 1, -1.5]} position2={[0, 1.5, -1]} rotation1={[0, Math.PI / 2, 0]} rotation2={[0, 0, Math.PI / 2]} />
+            <EdgePiece position1={[0, -1, 1.5]} position2={[0, -1.5, 1]} rotation1={[0, Math.PI / 2, 0]} rotation2={[0, 0, Math.PI / 2]} />
+            <EdgePiece position1={[0, -1, -1.5]} position2={[0, -1.5, -1]} rotation1={[0, Math.PI / 2, 0]} rotation2={[0, 0, Math.PI / 2]} />
 
-        <CornerPiece />
-    </>
+            <CornerPiece position1={[1.5, 1, 1]} position2={[1, 1, 1.5]} position3={[1, 1.5, 1]} />
+            <CornerPiece position1={[-1.5, 1, 1]} position2={[-1, 1, 1.5]} position3={[-1, 1.5, 1]} />
+            <CornerPiece position1={[1.5, -1, 1]} position2={[1, -1, 1.5]} position3={[1, -1.5, 1]} />
+            <CornerPiece position1={[-1.5, -1, 1]} position2={[-1, -1, 1.5]} position3={[-1, -1.5, 1]} />
+
+            <CornerPiece position1={[1.5, 1, -1]} position2={[1, 1, -1.5]} position3={[1, 1.5, -1]} />
+            <CornerPiece position1={[-1.5, 1, -1]} position2={[-1, 1, -1.5]} position3={[-1, 1.5, -1]} />
+            <CornerPiece position1={[1.5, -1, -1]} position2={[1, -1, -1.5]} position3={[1, -1.5, -1]} />
+            <CornerPiece position1={[-1.5, -1, -1]} position2={[-1, -1, -1.5]} position3={[-1, -1.5, -1]} />
+        </>
     )
 }
