83a5b45c83a51e3e5ea210a833142f638cb96ade
Jaro32768 <71781871+Jaro32768@users.noreply.github.com>
2022-10-09 13:19:00 +0200

    F moves are working! (well... almost)

--- src/components/CornerPiece.js
+++ src/components/CornerPiece.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 import PieceSide from './PieceSide';
 
 export default function CornerPiece(props) {
     return (
         <>
-            <PieceSide color={props.color1} position={props.position1} rotation={[Math.PI / 2, 0, 0]} />
-            <PieceSide color={props.color2} position={props.position2} rotation={[0, Math.PI / 2, 0]} />
-            <PieceSide color={props.color3} position={props.position3} rotation={[0, 0, Math.PI / 2]} />
+            <PieceSide color={props.color1} position={props.position1} rotation={props.rotation1} /> {/* [Math.PI / 2, 0, 0] */}
+            <PieceSide color={props.color2} position={props.position2} rotation={props.rotation2} /> {/* [0, Math.PI / 2, 0] */}
+            <PieceSide color={props.color3} position={props.position3} rotation={props.rotation3} /> {/* [0, 0, Math.PI / 2] */}
         </>
-    )
+    );
 }
