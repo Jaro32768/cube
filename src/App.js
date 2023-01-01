7626f23fd9042979eb90946eb5ef89af6d595613
Jaro32768 <71781871+Jaro32768@users.noreply.github.com>
2023-01-01 18:35:39 +0100

    Implement login system

--- src/App.js
+++ src/App.js
@@ -4,19 +4,29 @@ import Cube3x3x3 from './components/Cube3x3x3';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import LoginForm from './components/LoginForm';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
+    React.useEffect(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+            setIsLoggedIn(!!user);
+        });
+    }, []);
+    if (!isLoggedIn) {
+        return <LoginForm />;
+    }
     return (
-        <>
-            {/*             <LoginForm></LoginForm> */}
-            <div className='canvas-container'>
-                <Canvas className='canvas'>
-                    <OrbitControls enableZoom={false} enableDamping={false} enablePan={false} />
-                    <ambientLight intensity={0.5} />
-                    <Cube3x3x3></Cube3x3x3>
-                </Canvas>
-            </div>
-        </>
+        <div className='canvas-container'>
+            <Canvas className='canvas'>
+                <OrbitControls enableZoom={false} enableDamping={false} enablePan={false} />
+                <ambientLight intensity={0.5} />
+                <Cube3x3x3 />
+            </Canvas>
+        </div>
     );
 }
 
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,42 +1,48 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import '../css/LoginForm.css';
+import firebaseConfig from '../firebaseConfig.json';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+firebase.initializeApp(firebaseConfig);
 
 export default function LoginForm() {
-    const login = useRef(null);
-    const password = useRef(null);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
 
-    const users = [
-        ['aaa', 'aaa'],
-        ['bbb', 'bbb'],
-        ['ccc', 'ccc'],
-    ];
-
-    const onClick = (e) => {
-        e.preventDefault();
-        let loggedIn = false;
-        users.forEach((user) => {
-            if (user[0] === login.current.value && user[1] === password.current.value) {
-                console.log('logged in!');
-                loggedIn += 1;
-            }
-        });
-        if (!loggedIn) console.log('incorrect login or password');
+    const handleLogin = () => {
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .catch((error) => setError(error));
     };
+
+    const handleSignUp = () => {
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(email, password)
+            .catch((error) => setError(error));
+    };
+
     return (
-        <>
-            <form>
-                <div className='form-child'>
-                    <p>LOGIN</p>
-                    <input ref={login} placeholder='login' />
-                </div>
-                <div className='form-child'>
-                    <p>PASSWORD</p>
-                    <input ref={password} type='password' placeholder='password' />
-                </div>
-                <div className='form-child'>
-                    <button onClick={onClick}>LOGIN</button>
-                </div>
-            </form>
-        </>
+        <form>
+            <label>
+                Email:
+                <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+            </label>
+            <label>
+                Password:
+                <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+            </label>
+            {error && <p>{error.message}</p>}
+            <button type='button' onClick={handleLogin}>
+                Login
+            </button>
+            <button type='button' onClick={handleSignUp}>
+                Sign Up
+            </button>
+        </form>
     );
 }
