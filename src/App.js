94e9737534aa999ea31bc92ed531c992bcf1cf2d
Jaro32768 <71781871+Jaro32768@users.noreply.github.com>
2023-01-03 18:11:23 +0100

    Add info button

--- src/App.js
+++ src/App.js
@@ -16,6 +16,12 @@ function App() {
         setIsLoggedIn(false);
     };
 
+    const infoClicked = () => {
+        alert(
+            'This is a 3x3x3 Rubik\'s Cube. You can rotate the cube by dragging the mouse or you can use your keyboard (x, y, z). Difference is that by rotating cube with your mouse you only change where are you looking from on the cube, if you want to "regrip" the cube, you have to use keys. If you want to rotate the cube in a specific direction, you have to use your keyboard and it follows basic Rubik\'s cube notation (f - front, b - back, u - up, d - down, l - left, r - right, m - middle, e - equator, s - standing). That rotates sides clockwise. If you want to rotate cube in opposite direction, you can use capslock.'
+        );
+    };
+
     useEffect(() => {
         firebase.auth().onAuthStateChanged((user) => {
             setIsLoggedIn(!!user);
@@ -34,6 +40,9 @@ function App() {
 
             <button className='logout-btn' onClick={logoutClicked}>
                 Logout
+            </button>
+            <button className='info-btn' onClick={infoClicked}>
+                i
             </button>
         </div>
     );
