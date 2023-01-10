a50f03e505ab18e17390c1a80c3caba892e1e0d8
Jaro32768 <71781871+Jaro32768@users.noreply.github.com>
2023-01-10 09:10:54 +0100

    Add reusable Popup component

---
+++ src/components/Popup.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../css/Popup.css';
+
+export default function Popup(props) {
+    const hideAlert = () => {
+        props.setIsInfoVisible(false);
+    };
+
+    return (
+        <div className='alert-content'>
+            <div className='alert-title'>{props.title}</div>
+            <div className='alert-text'>{props.text}</div>
+            <div className='alert-buttons'>
+                <button className='alert-button' onClick={() => props.clicked()}>
+                    Got it!
+                </button>
+            </div>
+        </div>
+    );
+}
