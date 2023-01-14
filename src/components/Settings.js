adf7b401f24b513aa13b56d4d58fc19ef00e7353
Jaro32768 <71781871+Jaro32768@users.noreply.github.com>
2023-01-14 16:42:21 +0100

    give settings general structure

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -5,29 +5,25 @@ export default function Settings(props) {
     return (
         <>
             <h1>Settings</h1>
-            <h3>lorem</h3>
-            <select name='lorem' id='lorem'>
-                <option value='lorem'>lorem</option>
-                <option value='lorem'>lorem</option>
-                <option value='lorem'>lorem</option>
-                <option value='lorem'>lorem</option>
+            <h2>Presets</h2>
+            <select name='method'>
+                <option value='beginner'>Beginner method</option>
+                <option value='bcfop'>Beginner CFOP</option>
+                <option value='cfop'>CFOP</option>
+                <option value='custom'>Custom</option>
             </select>
-            <h3>lorem</h3>
-            <select name='lorem' id='lorem'>
-                <option value='lorem'>lorem</option>
+            <h2>First two layers</h2>
+            <select name='f2l'>
+                <option value='layer by layer'>Layer by layer</option>
+                <option value='bf2l'>Beginner F2L</option>
+                <option value='f2l'>F2L</option>
             </select>
-            <h3>lorem</h3>
-            <select name='lorem' id='lorem'>
-                <option value='lorem'>lorem</option>
+            <h2>Last layer</h2>
+            <select name='ll'>
+                <option value='beginner'>Beginner method</option>
+                <option value='ollpll'>OLL and PLL</option>
             </select>
-            <h3>lorem</h3>
-            <select name='lorem' id='lorem'>
-                <option value='lorem'>lorem</option>
-            </select>
-            <h3>lorem</h3>
-            <select name='lorem' id='lorem'>
-                <option value='lorem'>lorem</option>
-            </select>
+            <p>Keep in mind that as a guest you won't be able to restore your settings on your future site visit.</p>
             <button className='close-bttn' onClick={() => props.setAreSettingsVisible(false)}>
                 Back
             </button>
