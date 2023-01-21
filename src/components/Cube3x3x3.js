3d58c0bcf28961dac54c6b5fe0f9438aa778e5ba
Jaro32768 <71781871+Jaro32768@users.noreply.github.com>
2023-01-21 13:58:54 +0100

    correctly orient entire cube

--- src/components/Cube3x3x3.js
+++ src/components/Cube3x3x3.js
@@ -60,8 +60,15 @@ export default function Cube3x3x3(props) {
         else if (JSON.stringify(centerPiecesPositions[2]) === JSON.stringify([0, 1.5, 0])) setHintText('get white center on the bottom (X2)');
         else if (JSON.stringify(centerPiecesPositions[2]) === JSON.stringify([1.5, 0, 0])) setHintText('get white center on the bottom (Z)');
         else if (JSON.stringify(centerPiecesPositions[2]) === JSON.stringify([-1.5, 0, 0])) setHintText("get white center on the bottom (Z')");
-        else setHintText('white center is on the bottom');
-
+        else {
+            // get blue center on the front causing entire cube being oriented correctly
+            console.log(centerPiecesPositions[0]);
+            if (JSON.stringify(centerPiecesPositions[0]) === JSON.stringify([-1.5, 0, 0])) setHintText('get blue center on the front (Y)');
+            else if (JSON.stringify(centerPiecesPositions[0]) === JSON.stringify([0, 0, 1.5])) setHintText('get blue center on the front (Y2)');
+            else if (JSON.stringify(centerPiecesPositions[0]) === JSON.stringify([1.5, 0, 0])) setHintText('get blue center on the front (Y)');
+            else {
+            }
+        }
         if (!wasChangedAtlestOnce) return;
         for (const [, SIDE] of Object.entries(indexes)) {
             let index, value;
