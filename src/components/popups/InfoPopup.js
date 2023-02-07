e14a67c19af363479794534d3d134b601d2fdf27
Jaro32768 <71781871+Jaro32768@users.noreply.github.com>
2023-02-07 20:35:09 +0100

    Update InfoPopup text

--- src/components/popups/InfoPopup.js
+++ src/components/popups/InfoPopup.js
@@ -12,10 +12,10 @@ export default function InfoPopup(props) {
             <Popup
                 title='This is a 3x3x3 Rubik`s Cube'
                 text='You can rotate the cube by dragging the mouse or you can use your keyboard (x, y, z). Difference is that by rotating cube with your mouse you
-                only change where are you looking from on the cube, if you want to "regrip" the cube, you have to use keys. If you want to rotate the cube in a
+                only change where are you looking on the cube from. If you want to "regrip" the cube, you have to use keys. If you want to rotate the cube in a
                 specific direction, you have to use your keyboard and it follows basic Rubik`s cube notation (f - front, b - back, u - up, d - down, l - left, r
                 - right, m - middle, e - equator, s - standing). That rotates sides clockwise. If you want to rotate cube in opposite direction, you can use
-                capslock.'
+                capslock. Buttons on the bottom of the page are colored to make it easier for you to remember which side is which.'
                 clicked={() => hideAlert()}
             />
         </div>
